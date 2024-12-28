2ef7921f343158019db9efa929f867affbb20f1a
karthik <karthikunnam123@gmail.com>
2024-12-28 17:19:13 +0530

    sixth commit

--- infra/setup/outputs.tf
+++ infra/setup/outputs.tf
@@ -3,6 +3,6 @@ output "ecr_repo_url_app" {
     value = aws_ecr_repository.app.repository_url
 }
 output "ecr_repo_url_proxy" {
-    description = "app-repo-url"
-    value = aws_ecr_repository.PROXY.repository_url
+    description = "proxy-repo-url"
+    value = aws_ecr_repository.proxy.repository_url
 }
